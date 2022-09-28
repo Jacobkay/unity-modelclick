5ea056cb26357d405fbd343985c0d86da988c7d9
Jacobkay <582110415@qq.com>
2022-09-28 17:45:45 +0800

    修改demo，引用命名空间

--- Demo/ModelClick.cs
+++ Demo/ModelClick.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ZTools;
 
 public class ModelClick : MonoBehaviour
 {
-    
+
     void Start()
     {
         ModelClickTool.Instance.ModelClick += obj =>
